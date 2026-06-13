2e0a45abcdd5dab478f262180cf60ea11661bad9
zdg <zdg@cinema.com>
2026-06-13 15:56:49 +0800

    板块8：集成Nacos注册中心+Actuator健康检查+Docker Compose编排

--- backend/src/main/java/com/cinema/CinemaApplication.java
+++ backend/src/main/java/com/cinema/CinemaApplication.java
@@ -3,11 +3,12 @@ package com.cinema;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.cinema.mapper")
 public class CinemaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
     }
